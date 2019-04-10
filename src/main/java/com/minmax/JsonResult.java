3571eaee066290d738e8f2e674dd2085aa5e8915
胡唯_Sole <ben531@sina.com>
2019-04-10 08:40:42 +0800

    泛型

--- src/main/java/com/minmax/JsonResult.java
+++ src/main/java/com/minmax/JsonResult.java
@@ -3,35 +3,49 @@ package com.minmax;
 import org.springframework.stereotype.Component;
 
 @Component
-public class JsonResult {
+public class JsonResult<T> {
 	private String code;
-	private Object data;
+	private T data;
 
 	
 	public JsonResult() {
 	}
 	
 
-	public JsonResult(String code, Object data) {
+
+	public JsonResult(String code, T data) {
 		this.code = code;
 		this.data = data;
 	}
+
 
 
 	public String getCode() {
 		return code;
 	}
 
+
 	public void setCode(String code) {
 		this.code = code;
 	}
 
-	public Object getData() {
+
+	public T getData() {
 		return data;
 	}
 
-	public void setData(Object data) {
+
+	public void setData(T data) {
 		this.data = data;
 	}
+
+
+
+	@Override
+	public String toString() {
+		return "JsonResult [code=" + code + ", data=" + data + "]";
+	}
+	
+
 
 }
